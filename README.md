c10a1c755627ea0e677c072cc0b523d08cdf5f9c
Erle Carrara <carrara.erle@gmail.com>
2018-09-28 17:49:07 -0300

    Keep it simple, stupid

--- README.md
+++ README.md
@@ -31,7 +31,31 @@ tox
 Example
 -------
 
-    # settings.py
-    vault = Vault("myproject/email_secrets")
-    email_user = vault.get("EMAIL_USER")
-    email_password = vault.get("EMAIL_PASS")
+```python
+vault = Vault("myproject/email_secrets")
+email_user = vault.get("EMAIL_USER")
+email_password = vault.get("EMAIL_PASS")
+```
+
+```python
+OVERRIDE = {
+    'EMAIL_USER': 'bart.simpsons@example.com'
+}
+
+vault = Vault("myproject/email_secrets", look_first=OVERRIDE)
+email_user = vault.get('EMAIL_USER')
+assert email_user == 'bart.simpsons@example.com'
+```
+
+```python
+def my_super_special_get_config_fn(name):
+    if name == 'FRUIT':
+        return 'avocado'
+
+
+vault = Vault("myproject/email_secrets", look_first=my_super_special_get_config_fn)
+email_user = vault.get('EMAIL_USER')
+fruit = vault.get('FRUIT')
+
+assert fruit == 'avocado'
+```
---
+++ tests/test_vault.py
@@ -0,0 +1,68 @@
+import json
+try:
+    from unittest import mock
+except:
+    import mock
+
+from awsvault import Vault
+
+
+def test_should_return_secrets_from_aws_secrets_manager():
+    with mock.patch('awsvault.boto3') as boto3:
+        boto3.client().get_secret_value.return_value = {
+            'SecretString': json.dumps({
+                'EMAIL_USER': 'user-secret',
+                'EMAIL_PASS': 'user-pass'
+            })
+        }
+
+        vault = Vault("myproject/email_secrets")
+
+    email_user = vault.get("EMAIL_USER")
+    email_password = vault.get("EMAIL_PASS")
+
+    assert email_user == "user-secret"
+    assert email_password == "user-pass"
+
+
+def test_override_secrets():
+    with mock.patch('awsvault.boto3') as boto3:
+        boto3.client().get_secret_value.return_value = {
+            'SecretString': json.dumps({
+                'EMAIL_USER': 'user-secret',
+                'EMAIL_PASS': 'user-pass'
+            })
+        }
+
+        override = {
+            'EMAIL_USER': 'bart.simpsons@example.com'
+        }
+        vault = Vault("myproject/email_secrets", look_first=override)
+
+    email_user = vault.get("EMAIL_USER")
+    email_password = vault.get("EMAIL_PASS")
+
+    assert email_user == "bart.simpsons@example.com"
+    assert email_password == "user-pass"
+
+
+def test_override_secrets_using_fn():
+    with mock.patch('awsvault.boto3') as boto3:
+        boto3.client().get_secret_value.return_value = {
+            'SecretString': json.dumps({
+                'EMAIL_USER': 'user-secret',
+                'EMAIL_PASS': 'user-pass'
+            })
+        }
+
+        def override(name):
+            if name == 'EMAIL_PASS':
+                return 'test-override-fn'
+
+        vault = Vault("myproject/email_secrets", look_first=override)
+
+    email_user = vault.get("EMAIL_USER")
+    email_password = vault.get("EMAIL_PASS")
+
+    assert email_user == "user-secret"
+    assert email_password == "test-override-fn"
